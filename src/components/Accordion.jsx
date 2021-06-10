8acc45a85294b1a94e37d8684e9b92292d942a09
its-Saab <musaababs@gmail.com>
2021-06-10 22:27:27 +0200

    Add Accordion widget

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,32 @@
+//npm packages
+import React, { useState } from "react";
+//project files
+
+const Accordion = ({ items }) => {
+	const [activeIndex, setActiveIndex] = useState(null);
+	const onTitleClick = (index) => {
+		setActiveIndex(index);
+	};
+	const renderedList = items.map((item, index) => {
+		const active = index === activeIndex ? "active" : "";
+		return (
+			<React.Fragment key={index}>
+				<div onClick={() => onTitleClick(index)} className={`title ${active}`}>
+					<i className="dropdown icon"></i>
+					{item.title}
+				</div>
+				<div className={`content ${active}`}>
+					<p>{item.content}</p>
+				</div>
+			</React.Fragment>
+		);
+	});
+	return (
+		<div className="ui styled accordion">
+			{renderedList}
+			{activeIndex}
+		</div>
+	);
+};
+
+export default Accordion;
