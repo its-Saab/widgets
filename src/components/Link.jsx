85f8b5c5404e5b6267c9b344794d6e3bdef9a069
its-Saab <musaababs@gmail.com>
2021-06-14 09:41:46 +0200

    Add native Link component

---
+++ src/components/Link.jsx
@@ -0,0 +1,18 @@
+const Link = ({ href, className, children }) => {
+	const handleClick = (event) => {
+		if (event.metaKey || event.ctrlKey) {
+			return;
+		}
+		event.preventDefault();
+		window.history.pushState({}, "", href);
+
+		const navEvent = new PopStateEvent("popstate");
+		window.dispatchEvent(navEvent);
+	};
+	return (
+		<a onClick={handleClick} href={href} className={className}>
+			{children}
+		</a>
+	);
+};
+export default Link;
